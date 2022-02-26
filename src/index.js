1762439e67bfb6142ff83ed412b212d06f3d79f1
Swetha-Hirge <swethahirge61@gmail.com>
2022-02-26 22:03:23 +0530

    mongo-assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://swetha:MrY5zQ2rHMae802L@cluster0.0xins.mongodb.net/swetha123?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
