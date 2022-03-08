87c4430410f885f35a9aefb618750d015e9f474e
Swetha-Hirge <swethahirge61@gmail.com>
2022-03-09 02:03:19 +0530

    assignment/08/03

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,6 +15,7 @@ const loginUser = async function (req, res) {
   let userName = req.body.emailId;
   let password = req.body.password;
 
+
   let user = await userModel.findOne({ emailId: userName, password: password });
   if (!user)
     return res.send({
@@ -31,8 +32,6 @@ const loginUser = async function (req, res) {
   let token = jwt.sign(
     {
       userId: user._id.toString(),
-      batch: "thorium",
-      organisation: "FUnctionUp",
     },
     "functionup-thorium"
   );
@@ -41,13 +40,7 @@ const loginUser = async function (req, res) {
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
+  let token = req.headers["x-auth-token"];
   
   // If a token is present then decode the token with verify function
   // verify takes two inputs:
@@ -81,10 +74,29 @@ const updateUser = async function (req, res) {
 
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
+  res.send({ status: userData, data: updatedUser });
 };
+
+
+const deleteUser = async function (req, res) {
+  // Do the same steps here:
+  // Check if the token is present
+  // Check if the token present is a valid token
+  // Return a different error message in both these cases
+  
+    let userId = req.params.userId;
+    console.log(userId)
+    let user = await userModel.findById(userId);
+    //Return an error if no user with the given id exists in the db
+    if (!user) {
+      return res.send("No such user exists");
+    }
+let deletedUser = await userModel.findOneAndUpdate({ _id: userId },{$set:{isDelete:true}});
+    res.send({ status: deletedUser, data: deletedUser });
+  };
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser=deleteUser;
